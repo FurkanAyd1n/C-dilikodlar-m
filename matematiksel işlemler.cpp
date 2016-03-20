5bf6d4d49c089e77e6347676bab0f8a5234d5e45
FurkanAyd1n <furkan_kader_keita29@hotmail.com>
2016-03-20 15:29:28 +0200

    Added files via upload

---
+++ matematiksel işlemler.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int main()
+{
+	/*int sayi1=5,sayi2=7,toplam,fark,carpim;
+	toplam= sayi1+sayi2;
+	fark=sayi1-sayi2;
+	carpim=sayi1*sayi2;
+	float bolum=(float)sayi1/(float)sayi2;
+	printf("Toplam =  %d\n",toplam);
+	printf("Fark = %d\n",fark);
+	printf("Carpim = %d\n",carpim);
+	printf("Bolum = %.2f",bolum);
+	
+	int sayi1,sayi2;
+	
+	printf ("1.sayiyi giriniz :"); scanf("%d",&sayi1);
+	printf ("2 sayiyi giriniz :"); scanf("%d",&sayi2);
+	
+   int toplam =sayi1+sayi2;
+     int fark =sayi1-sayi2;
+       int carpim =sayi1*sayi2;
+        float bolum =(float)sayi1/(float)sayi2;
+        
+   
+    
+      printf("Sayilarin toplami : %d\nsayilarin farki : %d\nsayilarin carpimi : %d\nsayilarin bolumu %.2f",toplam,fark,carpim,bolum);
+    */
+	
+	int sayi;
+	printf("Sayi giriniz :"); scanf("%d",&sayi);
+    int kare=sayi*sayi;
+	int kup=sayi*sayi*sayi;
+	printf("Girilen sayinin karesi= %d\nkupu=%d' dir",kare,kup);
+	
+		   
+    
+	
+	
+	
+	
+}
---
+++ matematiksel işlemler2.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	// mod alma %
+ /*	int bolunen=10,bolum=3,kalan;
+	kalan=bolunen%bolum;
+	printf("Kalan :%d",kalan);
+	
+	int bolunen,bolum;
+		printf("Bolunen degeri giriniz :"); scanf("%d",&bolunen);
+	    printf("Bolum degeri giriniz :"); scanf("%d",&bolum);
+		int kalan=bolunen%bolum;
+		printf("Kalan :%d",kalan); 
+		
+		
+		// Dairenin alanýný bulma pi*r2;
+		float pi=3.14;
+		int r=5;
+		float alan=pi*r*r;
+		printf("Dairenin Alani :%f",alan);
+		
+		
+		// Dairenin alaný bulma pi*r2
+		float pi=3.14;
+		int yaricap;
+		printf("Yaricapi giriniz :"); scanf("%d",&yaricap);
+		float alan=pi*yaricap*yaricap;
+		printf ("Dairenin alani :%.2f",alan);
+		
+		
+		int abs(x) //x tamsayýsýnýn mutlak deðerini hesaplar
+		double fabs(x) // x gerçel sayýsýnýn mutlak deðerini hesaplar
+		double sqrt(x) //x sayýsýnýn karekökünü hesaplar
+		double pow(x,y) //x üzeri y deðerini hesaplar üs alýr
+		double log()  //pozitif x sayýsýnýn doðal logaritmasýný hesaplar, ln (x)
+		double log10() // pozitif x sayýsýnýn 10 tabanýndaki logaritmasýný hesaplar
+		
+		printf("-4 un mutlak degeri :%d\n",abs(-4));
+		printf("-4.5 in mutlak degeri :%.1f\n",fabs(-4.5));
+		printf("4 un karekoku :%.1f\n",sqrt(4));
+		printf("125 ussu 3 isleminin sonucu :%.1f\n",pow(125,3));	
+		printf("log 1 degeri :%.1f\n",log(1));
+		printf("log 10 tabaninda 10 degeri :%.1f",log10(10));
+		
+		
+			//hipotenüs bulma örneði c(2)=a2+b2;
+			int a=3,b=4,hipotenus;
+			hipotenus= a*a+b*b;
+			printf("Hipotenus :%.0f",sqrt(hipotenus));
+			*/
+			int a,b;
+			
+			printf("a degeri :"); scanf("%d",&a);
+			printf("b degeri :"); scanf("%d",&b);
+			double c=a*a+b*b;
+			printf("Hipotenus :%.0f",sqrt(c));
+			
+			
+			
+			
+			
+		
+		
+		
+		
+		
+		
+		
+		
+	    
+	
+	
+}
